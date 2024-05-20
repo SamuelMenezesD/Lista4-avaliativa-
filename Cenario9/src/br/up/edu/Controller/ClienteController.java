1655c8e57fcce24a8d29375bf7040643441a1a30
JoaoLeich <joaopedroleichnioski@gmail.com>
2024-05-20 18:02:36 -0300

    Terminando 9

--- Cenario9/src/br/up/edu/Controller/ClienteController.java
+++ Cenario9/src/br/up/edu/Controller/ClienteController.java
@@ -1,7 +1,107 @@
 package br.up.edu.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.up.edu.Model.ClienteEmpresa;
+import br.up.edu.Model.ClientePessoa;
+
 public class ClienteController {
 
-    
+    private static List<ClienteEmpresa> ClientesEmpresa = new ArrayList<>();
+
+    private static List<ClientePessoa> ClientesPessoa = new ArrayList<>();
+
+    public static void AdicionarClientePessoa(ClientePessoa cliente){
+
+        ClientesPessoa.add(cliente);
+
+    }
+
+    public static void AdicionarClientesEmpresa(ClienteEmpresa cliente){
+
+        ClientesEmpresa.add(cliente);
+
+    }
+
+    public static void ListarClientesPessoa(){
+
+        for (ClientePessoa clientePessoa : ClientesPessoa) {
+            
+            System.out.println(clientePessoa);
+
+        }
+
+    }
+
+
+    public static void ListarClientesEmpresa(){
+
+        for (ClienteEmpresa clienteEmpresa : ClientesEmpresa) {
+            
+            System.out.println(clienteEmpresa);
+
+        }
+
+    }
+
+    public static void EmprestarClientePessoa(double Valor,String CpfReferente){
+
+        for (ClientePessoa pessoa : ClientesPessoa) {
+            
+            if(pessoa.getCPF().equalsIgnoreCase(CpfReferente)){
+
+                pessoa.Emprestar(Valor);
+
+            }
+
+        }
+
+    }
+
+    public static void EmprestarClienteEmpresa(double valor,String Cnpj){
+
+        for (ClienteEmpresa clienteEmpresa : ClientesEmpresa) {
+
+            if(clienteEmpresa.getCNPJ().equalsIgnoreCase(Cnpj)){
+
+                clienteEmpresa.Emprestar(valor);
+
+            }
+
+        }
+
+    }
+
+    public static void DevolucaoClientePessoa(double ValorDevolucao,String CpfReferente){
+
+        for (ClientePessoa pessoa : ClientesPessoa) {
+            
+            if(pessoa.getCPF().equalsIgnoreCase(CpfReferente)){
+
+                pessoa.Devolver(ValorDevolucao);
+
+            }
+
+        }
+        
+
+    }
+
+    public static void DevolucaoClienteEmpresa(double ValorDevolucao,String Cnpj){
+
+        for (ClienteEmpresa clienteEmpresa : ClientesEmpresa) {
+
+            if(clienteEmpresa.getCNPJ().equalsIgnoreCase(Cnpj)){
+
+                clienteEmpresa.Emprestar(ValorDevolucao);
+
+            }
+
+        }
+
+    }
+
+
 
 }
--- Cenario9/src/br/up/edu/View/Tela.java
+++ Cenario9/src/br/up/edu/View/Tela.java
@@ -1,7 +1,48 @@
 package br.up.edu.View;
 
+import java.util.Scanner;
+
+import br.up.edu.Controller.ClienteController;
+
 public class Tela {
+    
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void exibirMenu() {
+        boolean sair = false;
+
+        while (!sair) {
+            System.out.println("===== MENU =====");
+            System.out.println("1. Incluir Cliente Pessoa");
+            System.out.println("2. Incluir Cliente Empresa");
+            System.out.println("3. Mostrar Dados Cliente Pessoa");
+            System.out.println("4. Mostrar Dados Cliente Empresa");
+            System.out.println("5. Emprestar Para Cliente Pessoa");
+            System.out.println("6. Emprestar Para Cliente Empresa");
+            System.out.println("7. Devolucao de Cliente Pessoa");
+            System.out.println("8. Devolucao de Cliente Empresa");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
+            
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                
+                
+
+                ClienteController.AdicionarClientePessoa(null);
+
+        break;
+                
+                case 0:
+                    sair = true;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+    }
 
     
-
 }
